5fe327152de54b7f418812a6de9c74797441c320
marioalbornoz <mario.albornozc@utem.cl>
2020-09-04 16:46:07 -0400

    mostrando el resultado en la ui

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -6,6 +6,10 @@ export const Resultado = ({cotizacion}) => {
     return (
         <div>
             <p>El precio es: <span>{cotizacion.PRICE}</span></p>
+            <p>El precio mas alto del dia: <span>{cotizacion.HIGHDAY}</span></p>
+            <p>El precio mas bajo del dia: <span>{cotizacion.LOWHOUR}</span></p>
+            <p>Variacion ultimas 24 hrs: <span>{cotizacion.CHANGEPCT24HOUR}</span></p>
+            <p>Ultima actualizacion: <span>{cotizacion.LASTUPDATE}</span></p>
         </div>
     )
 }
