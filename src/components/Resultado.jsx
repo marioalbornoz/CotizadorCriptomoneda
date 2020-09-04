417515277edf5a8a7cf9f83f12413eb12e2955ed
marioalbornoz <mario.albornozc@utem.cl>
2020-09-04 17:12:25 -0400

    css a los datos obtenidos

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React from "react";
+import styled from "@emotion/styled";
+
+const ResultadoDiv = styled.div`
+    color: #fff;
+`;
+const Info = styled.p`
+    font-size:18px;
+
+    span{
+        font-weight:bold;
+    }
+
+`;
+
+const Precio = styled.p`
+    font-size:30px;
+
+    span{
+        font-weight:bold;
+    }
+
+`;
 
 export const Resultado = ({cotizacion}) => {
     if(Object.keys(cotizacion).length === 0) return null;
     console.log(cotizacion);
     return (
-        <div>
-            <p>El precio es: <span>{cotizacion.PRICE}</span></p>
-            <p>El precio mas alto del dia: <span>{cotizacion.HIGHDAY}</span></p>
-            <p>El precio mas bajo del dia: <span>{cotizacion.LOWHOUR}</span></p>
-            <p>Variacion ultimas 24 hrs: <span>{cotizacion.CHANGEPCT24HOUR}</span></p>
-            <p>Ultima actualizacion: <span>{cotizacion.LASTUPDATE}</span></p>
-        </div>
+        <ResultadoDiv>
+            <Precio>El precio es: <span>{cotizacion.PRICE}</span></Precio>
+            <Info>El precio mas alto del dia: <span>{cotizacion.HIGHDAY}</span></Info>
+            <Info>El precio mas bajo del dia: <span>{cotizacion.LOWHOUR}</span></Info>
+            <Info>Variacion ultimas 24 hrs: <span>{cotizacion.CHANGEPCT24HOUR}</span></Info>
+            <Info>Ultima actualizacion: <span>{cotizacion.LASTUPDATE}</span></Info>
+        </ResultadoDiv>
     )
 }
